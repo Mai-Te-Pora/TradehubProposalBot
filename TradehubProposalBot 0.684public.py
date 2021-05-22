1f9da56ff1d863373cd6ab24ce7bd2c4c54124f1
intsol <crypto@intsol.com.au>
2021-05-22 23:15:11 +1000

    Update TradehubProposalBot 0.684public.py

--- TradehubProposalBot 0.684public.py
+++ TradehubProposalBot 0.684public.py
@@ -11,110 +11,134 @@ import requests
 
 # PARAMETERS
 # ------------------------------------------------------------------------------
-testing = True 
-sleepTime = 300   #Time sleep (300 for 5 min)
-telegram_bot_token = '10987485:AAF8RrWYxQH6vzwyN3dmSflGT57_8'
-telegram_bot_chatID = '-43098739'
-    
+testing = True
+sleepTime = 300  # Time sleep (300 for 5 min)
+telegramBotToken = '<BOT_TOKEN>'
+telegramBotChatIDs = ['<CHAT-ID>']
+
+discordWebHooks = [
+    'https://discord.com/api/webhooks/<channel-webhook>',
+    ]
+
 # CONSTANTS
 # ------------------------------------------------------------------------------
 JSON_FILE = 'proposalpassed.json'
 
-proposalsStatus = "VotingPeriod"
+proposalStatus = "VotingPeriod"
 
-activeIdS = []
-currentIdS = []
-announcedIdS = []
-resultIdS = []
+activeIDs = []
+currentIDs = []
+announcedIDs = []
+resultIDs = []
 
 dataSave = {}
-dataSave["announcedIdS"] = announcedIdS
-dataSave["resultIdS"] = resultIdS
-dataSave["currentIdS"] = currentIdS
+dataSave["announcedIDs"] = announcedIDs
+dataSave["resultIDs"] = resultIDs
+dataSave["currentIDs"] = currentIDs
+
 
 # FUNCTIONS
 # ------------------------------------------------------------------------------
-def telegram_bot_sendtext(bot_message):
-    
-    bot_token = telegram_bot_token
-    bot_chatID = telegram_bot_chatID
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
-
-    response = requests.get(send_text)
+def telegramBotSendtext(botMessage):
+    botToken = telegramBotToken
+    for botChatID in telegramBotChatIDs:
+        sendText = 'https://api.telegram.org/bot' + botToken + '/sendMessage?chat_id=' + botChatID + '&parse_mode=Markdown&text=' + botMessage
+        response = requests.get(sendText)
 
     return response.json()
 
+
+def discordSendText(message):
+
+    data = {"content": message}
+
+    for hook in discordWebHooks:
+        try:
+            response = requests.post(hook, json=data)
+        except:
+            print('Error posting to discord')
+
+
 # TEST MODE
 # ------------------------------------------------------------------------------
-if testing == True:
+if testing:
     sleepTime = 5
     proposalStatus = "Passed"
-    print ('Test mode')
+    print('Test mode')
 
 # MAIN
 # ------------------------------------------------------------------------------
-print ('TradehubProposalBot b0.684')
+print('TradehubProposalBot b0.684')
 
-while True :
+while True:
     try:
-        #Pause between checks
-        time.sleep(sleepTime)   
+        # Pause between checks
+        time.sleep(sleepTime)
 
-        #Check for proposal
+        # Check for proposals
         with urllib.request.urlopen("https://tradescan.switcheo.org/gov/proposals") as url:
-            info = json.loads(url.read().decode())
+            proposals = json.loads(url.read().decode())
 
-            #Check active proposal ID's
-            activeProposal =[d for d in info["result"] if d['proposal_status'] == (proposalStatus)]
-            activeIdS = [] #Reset activeIdS
-            for d in activeProposal:
-                activeIdS.append(d['id'])
+            # Check active proposal ID's
+            activeProposals = [d for d in proposals["result"] if d['proposal_status'] == (proposalStatus)]
+            activeIDs = []  # Reset activeIDs
+            for d in activeProposals:
+                activeIDs.append(d['id'])
 
             # Try to load any existing saved data
             try:
                 with open(JSON_FILE, 'r') as file:
                     dataSave = json.load(file)
                     if testing == True:
-                        print ("Data loaded", dataSave)
+                        print("Data loaded", dataSave)
             except:
-                    print ("No data loaded")
+                print("No data loaded")
             pass
 
-            # Actualize data
-            for elem in dataSave["announcedIdS"]:
-                activeIdS.remove(elem)
+            # Remove the already announced proposals
+            for elem in dataSave["announcedIDs"]:
+                activeIDs.remove(elem)
 
-            proposalIdS = (activeIdS[0])       
-           
-            #Check if there is a new proposal
-            proposal =[d for d in info["result"] if d['proposal_status'] == (proposalStatus) if d['id'] == (proposalIdS)]
-            for d in proposal:
-                proposalIdS = (proposal[0]['id'])
-                proposalTitle = (proposal[0]['content']['value']['title'])
+            # get the earliest proposal
+            proposalsID = (activeIDs[0])
 
-                proposalText = f"New proposal available - {proposalIdS} - {proposalTitle} - please give your vote on: https://switcheo.org/governance/proposal/{proposalIdS}"
-                test = telegram_bot_sendtext(proposalText)
-                print (test)
+            # Check if there are any new proposals
+            newProposals = [d for d in proposals["result"] if d['proposal_status'] == (proposalStatus) if
+                            d['id'] == (proposalsID)]
+            for d in newProposals:
+                proposalId = (d['id'])
+                proposalTitle = (d['content']['value']['title'])
 
-                dataSave["announcedIdS"].append(proposalIdS)
-                dataSave["currentIdS"].append(proposalIdS)
+                proposalText = f"New proposal available - {proposalId} - {proposalTitle} - please vote [here](https://switcheo.org/governance/proposal/{proposalId})"
+                resp = telegramBotSendtext(proposalText)
+                print(resp)
 
-            #Check if a recent proposal has ended
-            endProposal =[d for d in info["result"] if d['proposal_status'] == "Passed" or d['proposal_status'] == "Rejected" if d['id'] in dataSave["currentIdS"]]
+                discordSendText(proposalText)
+
+                dataSave["announcedIDs"].append(proposalId)
+                dataSave["currentIDs"].append(proposalId)
+
+            # Check if a recent proposal has ended
+            endProposal = [d for d in proposals["result"] if
+                           d['proposal_status'] == "Passed" or d['proposal_status'] == "Rejected" if
+                           d['id'] in dataSave["currentIDs"]]
+
             for d in endProposal:
-                endProposalID = (endProposal[0]['id'])
+                endProposalId = (endProposal[0]['id'])
                 endProposalTitle = (endProposal[0]['content']['value']['title'])
-                
-                proposalText = f"Recent vote for proposal - {endProposalID} - {endProposalTitle} - Here is the results => https://switcheo.org/governance/proposal/{endProposalID} - {d['proposal_status']} <- Thanks for your participation"
-                test = telegram_bot_sendtext(proposalText)
-                print (test)
 
-                dataSave["resultIdS"].append(d['id'])
-                dataSave["currentIdS"].remove(d['id'])
+                proposalText = f"Recent vote for proposal - {endProposalId} - {endProposalTitle} has ended.\n\nThe results are [here](https://switcheo.org/governance/proposal/{endProposalId})\n\n The Proposal: {d['proposal_status']}\n\n Thanks for your participation"
+                resp = telegramBotSendtext(proposalText)
+                print(resp)
+
+                discordSendText(proposalText)
+
+                dataSave["resultIDs"].append(d['id'])
+                dataSave["currentIDs"].remove(d['id'])
 
             # Overwrite the saved data
             with open(JSON_FILE, 'w') as file:
                 json.dump(dataSave, file)
-               
+
     except:
         pass
